e40ee47711860735c5ccd775bf39658168501fd8
Balkrishna Bharti <balkrishnabharti3462@gmail.com>
2024-07-08 13:41:26 +0530

    changed

--- main.py
+++ main.py
@@ -2,16 +2,11 @@ import face_recognition
 import cv2
 import numpy as np
 
-# This is a demo of running face recognition on live video from your webcam. It's a little more complicated than the
+# This is a demo of running face recognition on live video from the webcam. It's a little more complicated than the
 # other example, but it includes some basic performance tweaks to make things run a lot faster:
 #   1. Process each video frame at 1/4 resolution (though still display it at full resolution)
 #   2. Only detect faces in every other frame of video.
 
-# PLEASE NOTE: This example requires OpenCV (the `cv2` library) to be installed only to read from your webcam.
-# OpenCV is *not* required to use the face_recognition library. It's only required if you want to run this
-# specific demo. If you have trouble installing it, try any of the other demos that don't require it instead.
-
-# Get a reference to webcam #0 (the default one)
 video_capture = cv2.VideoCapture(0)
 
 # Load a sample picture and learn how to recognize it.
@@ -30,7 +25,7 @@ d3_face_encoding = face_recognition.face_encodings(d3_image)[0]
 d4_image = face_recognition.load_image_file("Doctors/d4.png")
 d4_face_encoding = face_recognition.face_encodings(d4_image)[0]
 
-# Create arrays of known face encodings and their names
+# known face encodings and their names
 known_face_encodings = [
     d1_face_encoding,
     d2_face_encoding,
@@ -51,7 +46,6 @@ face_names = []
 process_this_frame = True
 
 while True:
-    # Grab a single frame of video
     ret, frame = video_capture.read()
 
     # Resize frame of video to 1/4 size for faster face recognition processing
